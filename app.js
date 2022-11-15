080835fdfb5ae77d1594fba3046c881b11ca62e4
FtpApoc <andrew.hughes0@me.com>
2022-11-15 11:06:54 +0000

    week 3 end

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
-const express = require('express')();
+const express = require('express');
 const path = require('path');
 const chalk = require('chalk');
 const debug = require('debug')('app');
 const morgan = require('morgan');
 
-const app = express;
+const app = express();
 app.use(morgan('tiny'))
+
+app.use(express.static(path.join(__dirname, '/public')));
+app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')))
+app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')))
+app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist')))
 
 app.get('/',function(req,res){
   res.sendFile(path.join(__dirname,'/views/index.html'));
