55de71108f15f6eb09a90a59500bca50de6ace4a
home <just the same>
2022-11-21 00:59:05 +0000

    Week 4 T3 EJS

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const debug = require('debug')('app');
 const morgan = require('morgan');
 
 const app = express();
+const port = process.env.PORT || 3000;
 app.use(morgan('tiny'))
 
 app.use(express.static(path.join(__dirname, '/public')));
@@ -16,6 +17,6 @@ app.get('/',function(req,res){
   res.sendFile(path.join(__dirname,'/views/index.html'));
 })
 
-app.listen(3000,function(){
-  debug('listening on port ' + chalk.green('3000'));
+app.listen(port,function(){
+  debug('listening on port ' + chalk.green(port));
 })
