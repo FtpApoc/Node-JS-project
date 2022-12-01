6ca38fb50014d816b06b7dec3d9dfe02399c44f8
home <2nd time lets gooo>
2022-12-01 18:44:12 +0000

    db header issue

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -6,27 +6,48 @@ const debug = require('debug')('app:adminRoutes');
 //initializing router for adminRouter
 const adminRouter = express.Router();
 
+const books = [
+  {
+    title: 'Sense and Sensibility',
+    genre: 'fiction',
+    author: 'Jane Austen',
+    read: false
+  },
+  {
+    title: 'The Sign of the Four',
+    genre: 'mystery',
+    author: 'Sir Artur Conan Doyle',
+    read: true
+  }
+]
+
 //initazlizing the route to house stacked functions for adminRouter
 adminRouter.route('/')
   .get((req,res) => {
     //const the URLs for where mongo is stored and what the DB is called
-    const url = 'mongodb://localhost27017';
+    const url = 'mongodb://localhost:27017';
     const dbname = 'LibraryApp';
 
     //start of async function
-    (async function mongo(){
+    (async function mongo() {
       //initialized
       let client;
-      try{
+      try {
         //async call to await connect
         client = await mongoClient.connect(url);
         //confirm connection in debug
         debug('connection to mongoDB server');
+
+        const db = client.db(dbname);
+        const response = await db.collection('books').insertMany(books);
+        res.json(response)
       }
       //catch part of try...catch
       catch(err){
-        debug(err)
+        debug("catch");
+        debug(err.stack);
       }
+      client.close();
     })()
     //HTML?
     res.send('inserting books');
