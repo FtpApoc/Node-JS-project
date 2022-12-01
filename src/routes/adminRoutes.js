9edf1b0e3d8cf54469517f3973c04332bd416343
home <2nd time lets gooo>
2022-12-01 14:20:42 +0000

    Setting Up MongoDB

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -1,6 +1,10 @@
 const express = require ('express');
 const adminRouter = express.Router();
 
+const mongoClient = require('mongodb').MongoClient;
+const url = 'mongodb://localhost27017';
+const dbname = 'LibraryApp';
+
 adminRouter.route('/')
   .get((req,res) => {
     res.send('inserting books');
